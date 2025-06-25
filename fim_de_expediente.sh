38265c3fc281c4c68bb0ec623919d2d369c5dd81
Kaê <kae.budke@gmail.com>
2025-06-25 20:40:04 +0200

    Update Repo | 25/06/25

---
+++ fim_de_expediente.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# 
+# Repositório no GitHub: https://github.com/budkee/sirius
+#
+# Alterar a mensagem conforme o que foi feito no dia 
+echo "\n # -------------- Git add -------------- #"
+git add .
+echo "\n # -------------- Git commit -------------- #"
+git commit -m "Update Repo | 25/06/25"
+echo "\n # -------------- Git Push -------------- #"
+git push
+echo "\n # -------------- Git Status -------------- #"
+git status
+
